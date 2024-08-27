9f641c296f57caed1e48460150aa844c89e63ac3
Raindrop-Inc <ashok@raindropinc.com>
2024-08-27 18:07:06 +0200

    Update hooks.py

--- otech/hooks.py
+++ otech/hooks.py
@@ -118,7 +118,7 @@ app_license = "MIT"
 
 doc_events = {
 	"Stock Entry": {
-		"on_update": "otech.otech.custom_code.stock_entry.py",
+		"on_update": "otech.otech.custom_code.stock_entry.on_save",
 		# "on_cancel": "method",
 		# "on_trash": "method"
 	}
