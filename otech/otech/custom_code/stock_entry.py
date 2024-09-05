a8f5943412ad8aaf9d2e08d70d5f32bd7970e817
Raindrop-Inc <ashok@raindropinc.com>
2024-09-05 12:06:05 +0545

    Update stock_entry.py

--- otech/otech/custom_code/stock_entry.py
+++ otech/otech/custom_code/stock_entry.py
@@ -6,5 +6,5 @@ def on_save(doc, method):
             frappe.db.set_value("Task wise Budget", task.name, "consumed_qty", item.qty)
             frappe.db.set_value("Task wise Budget", task.name, "remaining_qty", task.estimated_qty - item.qty)
             frappe.db.set_value("Task wise Budget", task.name, "remaining_", ((task.remaining_qty / task.estimated_qty) * 100))
-            # frappe.db.set_value("Task wise Budget", task.name, "remaining_", (100 - ((task.estimated_qty - item.qty) / task.estimated_qty) * 100))
+            # frappe.db.set_value("Task wise Budget", task.name, "remaining_", (100 - ((task.remaining_qty / task.estimated_qty) * 100))
             frappe.db.commit()                                                                                             
