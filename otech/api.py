03b9a29650c2e6ee6fdd5e3321f80944ad264964
Raindrop-Inc <ashok@raindropinc.com>
2024-09-07 20:33:33 +0200

    Update api.py

--- otech/api.py
+++ otech/api.py
@@ -1,16 +1,19 @@
 def attendance(employee, status, shift, attendance_date, log_type, datetime, late_entry=None, early_exit=None):
-    attendance = frappe.new_doc("Attendance")
-    attendance.employee = employee
-    attendance.status = status
-    attendance.shift = shift
-    attendance.attendance_date = attendance_date
-    attendance.docstatus = 1
-    attendance.insert()
-    frappe.db.commit()
-    employee_check_in = frappe.new_doc("Employee Checkin")
-    employee_check_in.employee = employee
-    employee_check_in.log_type = log_type
-    employee_check_in.datetime = datetime
-    employee_check_in.attendance = attendance.name
-    employee_check_in.insert()
-    frappe.db.commit()
+    try:
+        attendance = frappe.new_doc("Attendance")
+        attendance.employee = employee
+        attendance.status = status
+        attendance.shift = shift
+        attendance.attendance_date = attendance_date
+        attendance.docstatus = 1
+        attendance.insert()
+        frappe.db.commit()
+        employee_check_in = frappe.new_doc("Employee Checkin")
+        employee_check_in.employee = employee
+        employee_check_in.log_type = log_type
+        employee_check_in.datetime = datetime
+        employee_check_in.attendance = attendance.name
+        employee_check_in.insert()
+        frappe.db.commit()
+    except Exception as e:
+        print( f" {e} ")
