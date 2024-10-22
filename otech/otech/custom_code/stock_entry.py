2b2f4de8baf7e4056ac48b817192e2cfae41aebd
Raindrop-Inc <ashok@raindropinc.com>
2024-10-22 21:41:17 +0545

    Update stock_entry.py

--- otech/otech/custom_code/stock_entry.py
+++ otech/otech/custom_code/stock_entry.py
@@ -30,3 +30,20 @@ def on_save(doc, method):
     doc.consumed_qty_updated = True
     doc.save()
     frappe.db.commit()
+
+def on_cancel(doc, method):
+    # if doc.get("is_canceled"):  # Assuming 'is_canceled' is a field that indicates cancellation
+        for item in doc.items:
+            tasks = frappe.db.get_all("Task wise Budget", filters={"parent": item.custom_task, "item_name": item.item_code}, fields=["*"])
+            for task in tasks:
+                current_consumed_qty = task.consumed_qty or 0
+                # Reduce consumed_qty by the quantity of the canceled item
+                new_consumed_qty = current_consumed_qty - item.qty
+                remaining_qty = task.estimated_qty - new_consumed_qty
+                remaining_percentage = 100 - ((remaining_qty / task.estimated_qty) * 100)
+
+                frappe.db.set_value("Task wise Budget", task.name, "consumed_qty", new_consumed_qty)
+                frappe.db.set_value("Task wise Budget", task.name, "remaining_qty", remaining_qty)
+                frappe.db.set_value("Task wise Budget", task.name, "remaining_", remaining_percentage)
+        doc.save()
+        frappe.db.commit()
